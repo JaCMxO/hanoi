762a7dc5ba3a60e4ea6f71c0e2efc9651248036b
Jaime Alberto Camacho Ortiz <jaime.albertogdl@gmail.com>
2021-06-08 12:23:28 -0500

    hanoi 1st test

--- hanoi.c
+++ hanoi.c
@@ -10,8 +10,33 @@
 
 #include <stdio.h>
 
+#define N 3
+
+int A[N] = {3, 2, 1};
+int B[N] = {0};
+int C[N] = {0};
+
+
+int hanoi(int n, int source, int target);
+void pMove(int source, int target);
+
 int main()
 {
-
+	hanoi(N, 1, N);
 	return 0;
+}
+
+int hanoi(int n, int source, int target){
+	if(n == 1){		//base case
+		pMove(source, target);
+	}else{
+		int auxiliary = 6 - (source + target);
+		hanoi(n-1, source, target);
+		pMove(source, target);
+		hanoi(auxiliary, target);
+	}
+}
+
+void pMove(int source, int target){
+	printf("%d -> %d", source, target);
 }
