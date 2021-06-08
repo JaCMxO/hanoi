9601878fd8a5ab946d0ce2a17466087c7147e2f1
Jaime Alberto Camacho Ortiz <jaime.albertogdl@gmail.com>
2021-06-08 10:25:55 -0500

    initial commit

---
+++ hanoi.asm
@@ -0,0 +1,10 @@
+#hanoi.asm
+#Hanoi tower solution using MIPS assembler
+#author: Jaime Camacho
+
+.data			#data in memory
+
+.text			#programm code
+main:
+
+exit:
---
+++ hanoi.c
@@ -0,0 +1,17 @@
+/**
+ * @file hanoi.c
+ * 
+ * Hanoi towers solution algorith
+ * 
+ * @author Jaime Camacho
+ * Contact: jaime.albertogdl@gmail.com
+ * 
+ **/
+
+#include <stdio.h>
+
+int main()
+{
+	
+	return 0;
+}
