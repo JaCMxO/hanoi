ae2106a04ec5b6f8c2df07172b78c8e40facb66e
Jaime Alberto Camacho Ortiz <jaime.albertogdl@gmail.com>
2021-06-08 12:53:25 -0500

    hanoi 2nd test working with gui

--- hanoi.c
+++ hanoi.c
@@ -10,28 +10,32 @@
 
 #include <stdio.h>
 
-#define N 3
-
-int A[N] = {3, 2, 1};
-int B[N] = {0};
-int C[N] = {0};
-
+int count=0;
 
 int hanoi(int n, int source, int target, int aux);
+void pMoves(int source, int target);
 
 int main()
 {
-	hanoi(N, 1, 3, 2);
+	int n;
+	printf("Enter the number of disks: ");
+	scanf("%d", &n);
+	hanoi(n, 1, 3, 2);
+	printf("Number of moves: %d\n", count);
 	return 0;
 }
 
 int hanoi(int n, int source, int target, int aux){
-	int auxiliary;
 	if(n == 1){		//base case
-		printf("%d -> %d\n", source, target);
+		pMoves(source, target);
 	}else{
 		hanoi(n-1, source, aux, target);
-		printf("%d -> %d\n", source, target);
+		pMoves(source, target);
 		hanoi(n-1, aux, target, source);
 	}
+}
+
+void pMoves(int source, int target){
+	printf("%d -> %d\n", source, target);
+	count++;
 }
